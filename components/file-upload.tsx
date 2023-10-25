73e9dfbb13f634d9bc55222ce64475eef2edfdee
wuxiaoran <wuxiaoran@wjbrain.com>
2023-10-25 18:28:36 +0800

    feat: uploadthing

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,14 +1,23 @@
 "use client";
 import React from "react";
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
   value: string;
-  endpoint: "serverImage" | "messageImage";
+  endpoint: "serverImage" | "messageFile";
 }
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  return <div>FileUpload</div>;
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].fileUrl);
+      }}
+    />
+  );
 };
 
 export default FileUpload;
